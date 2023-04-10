2d4e5b90fb7d9c0c11cad83332118ecd72a0c7b6
wwdfdo <wwdfdo@gmail.com>
2023-04-10 16:57:17 +0530

    added new menu items for navbar

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,36 +6,49 @@ import circle from "../../images/circle.png";
 import { IoIosAppstore } from "react-icons/io";
 import { FaGooglePlay } from "react-icons/fa";
 import { HiDownload } from "react-icons/hi";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaDiscord } from "react-icons/fa";
 
 const Banner = () => {
   return (
     <div className=" bg-white h-screen px-2 py-5">
       <ul className="flex justify-between absolute right-0 left-0 gap-10 pb-5 bg-[#ed3b4b]  mx-20">
-        <div className="pt-5 font-bold text-xl">Crypko</div>
+        <div className="mt-5 font-bold text-xl text-black z-[100]">Crypko</div>
         <div className="flex justify-center  gap-20 px-40 py-4  z-100 nav-list absolute right-0 left-1 w-[70%] mx-auto font-bold">
-          <li>Guideline</li>
-          <li>Faq</li>
-          <li>Contact Us</li>
-          <li>Discord</li>
-          <li>Twitter</li>
+          <li>Home</li>
+          <li>Gallery</li>
+          <li>World</li>
+          <li>Roadmap</li>
+          <li>More</li>
+          <li>Buy</li>
         </div>
-        <div className="mt-5 ">
-          {" "}
-          <span className="bg-white px-5 py-2 rounded-3xl font-bold">
-            Join Us
-          </span>
+        <div className="mt-5 z-[100] ">
+          <ul className="flex   items-center ">
+            {/* <div className="h-6 w-[3px] bg-white"></div> */}
+
+            <li className="tracking-[.1825rem] font-bold text-[0.9rem] px-3 h-8 text-center rounded-md flex items-center cursor-pointer uppercase ">
+              <AiFillTwitterCircle size={24} />
+            </li>
+            <li className="tracking-[.1825rem] font-bold text-[0.9rem] px-3 h-8 text-center rounded-md flex items-center cursor-pointer uppercase ">
+              <AiFillInstagram size={24} />
+            </li>
+            <li className="tracking-[.1825rem] font-bold text-[0.9rem] px-3 h-8 text-center rounded-md flex items-center cursor-pointer uppercase ">
+              <FaDiscord size={24} />,
+            </li>
+          </ul>
         </div>
       </ul>
 
       <div className=" bg-[#ed3b4b] h-[95vh] w-full rounded-2xl flex flex-col relative ">
         <div className="flex justify-between w-full">
           <div className="flex flex-col w-3/5 ">
-            <h1 className="pt-20 pl-[10%] text-white font-bold text-7xl leading-tighter tracking-wider">
-              ANIME CHARACTER <br /> GENERATION
+            <h1 className="mt-20 pl-[10%] text-white font-bold text-8xl leading-tighter tracking-wider">
+              The Outlaw's Last Ride
             </h1>
             <img
               src={circle}
-              className="w-[200px] absolute right-[30%] bottom-[30%] self-end"
+              className="w-[200px] absolute right-[40%] bottom-[30%] self-end"
               alt=""
             />
           </div>
@@ -52,7 +65,7 @@ const Banner = () => {
           <div>
             <img
               src={crypkogirlboy}
-              className="w-[700px] absolute bottom-0 left-20 "
+              className="w-[600px] absolute bottom-0 left-20 "
               alt=""
             />
           </div>
