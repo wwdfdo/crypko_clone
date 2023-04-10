985310e88348d6c6b2fc4152a805569d05385e10
wwdfdo <wwdfdo@gmail.com>
2023-04-10 10:31:44 +0530

    added new bg color

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className=" bg-white h-screen px-2 py-5">
-      <ul className="flex justify-between absolute right-0 left-0 gap-10 pb-5 bg-[#117f95]  mx-20">
+      <ul className="flex justify-between absolute right-0 left-0 gap-10 pb-5 bg-[#ed3b4b]  mx-20">
         <div className="pt-5 font-bold text-xl">Crypko</div>
         <div className="flex bg-white gap-10 p-5 rounded-br-xl rounded-bl-xl">
           <li>Guideline</li>
@@ -19,7 +19,7 @@ function App() {
         </div>
       </ul>
 
-      <div className=" bg-[#117f95] h-[95vh] w-full rounded-2xl ">
+      <div className=" bg-[#ed3b4b] h-[95vh] w-full rounded-2xl ">
         <h1 className=" pt-[10%] pl-[10%] text-white font-extrabold text-6xl leading-tighter">
           ANIME CHARACTER <br /> GENERATION
         </h1>
