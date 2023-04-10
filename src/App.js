f4f4743b43f88bd0918fcadb19bd3e40d837b2d5
wwdfdo <wwdfdo@gmail.com>
2023-04-10 10:25:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-white h-screen px-2 py-5">
+      <ul className="flex justify-between absolute right-0 left-0 gap-10 pb-5 bg-[#117f95]  mx-20">
+        <div className="pt-5 font-bold text-xl">Crypko</div>
+        <div className="flex bg-white gap-10 p-5 rounded-br-xl rounded-bl-xl">
+          <li>Guideline</li>
+          <li>Faq</li>
+          <li>Contact Us</li>
+          <li>Discord</li>
+          <li>Twitter</li>
+        </div>
+        <div className="mt-5 ">
+          {" "}
+          <span className="bg-white px-5 py-2 rounded-3xl">Join Us</span>
+        </div>
+      </ul>
+
+      <div className=" bg-[#117f95] h-[95vh] w-full rounded-2xl ">
+        <h1 className=" pt-[10%] pl-[10%] text-white font-extrabold text-6xl leading-tighter">
+          ANIME CHARACTER <br /> GENERATION
+        </h1>
+      </div>
     </div>
   );
 }
